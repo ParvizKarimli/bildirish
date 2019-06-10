bac74b8f6e8b840d5f76c97728a09218ae48cead
Parviz Karimli <devparviz@gmail.com>
2019-06-10 12:46:33 +0400

    Removed login link from home index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,6 @@
     <div class="jumbotron text-center">
         <h1>Bildirish</h1>
         <p>Bildirish SMS sender application</p>
-        <p>
-            <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
-        </p>
     </div>
 
 @endsection
