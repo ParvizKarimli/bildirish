eec354b558517258e1115d1ba7bc0dcb213f5c5f
Parviz Karimli <devparviz@gmail.com>
2019-06-11 18:43:24 +0400

    Moved up credits/notify route above credits resource route so now it (notify) works

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,6 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
+Route::get('credits/notify', 'CreditsController@notify');
 Route::resource('credits', 'CreditsController');
 Route::post('credits/search', 'CreditsController@search');
-Route::get('credits/notify', 'CreditsController@notify');
