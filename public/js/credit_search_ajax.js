bf6d1fc219e45b70e43106bd3daaa58f15f43621
Parviz Karimli <devparviz@gmail.com>
2019-06-11 11:04:33 +0400

    Fixed if search_term empty what to display (display the whole table just as normal without any search term) in public/js/credit_search_ajax.js

--- public/js/credit_search_ajax.js
+++ public/js/credit_search_ajax.js
@@ -3,31 +3,25 @@
 function getCreditsBySearchTerm(search_term) {
     var search_term = document.getElementById('search_term').value;
 
-    if(search_term == '') {
-        // Fix this later
-        document.getElementById('credits_table').tBodies[0].innerHTML = 'empty';
-        return;
+    if(window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        var xmlhttp = new XMLHttpRequest();
     } else {
-        if(window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            var xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5
-            var xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-
-        xmlhttp.open('POST', '/credits/search', true);
-        // Send the proper header information along with the request
-        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.setRequestHeader('X-CSRF-TOKEN', document.querySelector('meta[name="csrf-token"]').content);
-        xmlhttp.send('search_term=' + search_term);
-
-        xmlhttp.onreadystatechange = function()
-        {
-            if(this.readyState == 4 && this.status == 200)
-            {
-                document.getElementById('credits_table').tBodies[0].innerHTML = this.responseText;
-            }
-        };
+        // code for IE6, IE5
+        var xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
+
+    xmlhttp.open('POST', '/credits/search', true);
+    // Send the proper header information along with the request
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.setRequestHeader('X-CSRF-TOKEN', document.querySelector('meta[name="csrf-token"]').content);
+    xmlhttp.send('search_term=' + search_term);
+
+    xmlhttp.onreadystatechange = function()
+    {
+        if(this.readyState == 4 && this.status == 200)
+        {
+            document.getElementById('credits_table').tBodies[0].innerHTML = this.responseText;
+        }
+    };
 }
